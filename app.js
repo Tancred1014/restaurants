728e8beb62d9142192cb9ace3cb2a72dc176716c
Tancred1014 <tancred.shih@gmail.com.tw>
2023-11-15 14:58:32 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ app.get('/restaurants', (req, res) => {
   res.render('index', { restaurants: restaurants })
 })
 
-app.get('/restaurant/:id', (req, res) => {
-  res.send(`read movie:${id}`)
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurants.find((restaurant) => restaurant.id.toString() === id)
+  res.render('detail', { restaurant })
 })
 
 app.listen(port, () => {
