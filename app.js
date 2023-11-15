f6b06ea70591efba2b7f89d813d32f379553f180
Tancred1014 <tancred.shih@gmail.com.tw>
2023-11-15 13:21:10 +0800

    add static files & json for model

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/restaurant_list')
+})
+
+app.get('/restaurant_list', (req, res) => {
+  res.send('listing restaurant')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  res.send(`read movie:${id}`)
 })
 
 app.listen(port, () => {
